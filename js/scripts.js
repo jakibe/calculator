de674a399499ef858c6343cedca4d57aad9f6be0
jakibe <65843157+jakibe@users.noreply.github.com>
2021-11-16 19:55:59 +0300

    commit index

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt('Enter No 1'));
+var number2 = parseInt(prompt('Enter No 2'));
+
+alert(add(number1,number2));
